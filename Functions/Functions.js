61c7ba56ae4ddfbc286a8f26a46bae7795e6c74b
MatchanJJ <jedzxcx1@gmail.com>
2024-09-19 14:29:20 +0800

    Database pool

---
+++ Functions/Functions.js
@@ -0,0 +1,24 @@
+import pool from './DBConnection.js';  // Use 'import' to get the connection pool
+
+async function runQuery() {
+    try {
+        const [rows, fields] = await pool.query("SELECT * FROM package");
+        console.log(rows);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+runQuery();
+
+async function findPackage(id) {
+    try {
+        const [rows, fields] = await pool.query("SELECT * FROM package WHERE id = ?",[id]);
+        console.log(rows);
+    } catch (error) {
+        console.error(error);
+    }
+}
+//findPackage('PKG001')
+
