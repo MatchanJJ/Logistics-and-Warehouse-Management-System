987c460df4b4e5c9e12cb3e66f12b0e49079dfcf
MatchanJJ <jedzxcx1@gmail.com>
2024-10-09 00:09:29 +0800

    ..

--- Functions/Main.js
+++ Functions/Main.js
@@ -25,7 +25,7 @@
 
     // Initialize Express
     const app = express();
-    const port = 6969;
+    const port = 8080;
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use((req, res, next) => {
