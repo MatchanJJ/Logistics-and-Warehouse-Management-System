28b629ded8abce701673c7a7757cf1dc5e6320e8
chicocaine <eduardanthony1208@gmail.com>
2024-10-09 20:02:00 +0800

    OrderService changes

--- test_main/services/OrderService.js
+++ test_main/services/OrderService.js
@@ -38,17 +38,16 @@ async function getOrderData(order_id) {
             SELECT
                 o.order_id,
                 CONCAT(c.customer_first_name, ' ', c.customer_last_name) AS customer_name,
+                o.order_date_time AS time_stamp,
                 os.order_status_name AS order_status,
-                s.shipping_service_name AS shipping_service,
-                o.shipping_address,
+                o.delivery_address,
                 o.shipping_receiver,
-                ot.order_type_name AS order_type,
+                s.shipping_service_name AS shipping_service,
                 o.order_total_amount
             FROM orders o
             JOIN customers c ON o.customer_id = c.customer_id
             JOIN order_status os ON o.order_status_id = os.order_status_id
             JOIN shipping_services s ON o.shipping_service_id = s.shipping_service_id
-            JOIN order_types ot ON o.order_type_id = ot.order_type_id
             WHERE o.order_id = ?;
         `, [order_id]);
         if (rows.length > 0) {
@@ -66,6 +65,17 @@ async function getOrderData(order_id) {
 // add postal order
 async function addPostalOrder(order_id, parcel_id) {
     try {
+        const [parcel] = await db.query(`
+            SELECT parcel_unit_price
+            FROM parcels
+            WHERE parcel_id = ?;
+        `, [parcel_id]);
+
+        if (!parcel.length) {
+            console.error('Parcel not found:', parcel_id);
+            return false; 
+        }
+        
         const [inventory] = await db.query(`
             SELECT parcel_id, warehouse_id
             FROM parcel_inventories
@@ -77,17 +87,6 @@ async function addPostalOrder(order_id, parcel_id) {
             return false;
         }
 
-        const [parcel] = await db.query(`
-            SELECT parcel_unit_price
-            FROM parcels
-            WHERE parcel_id = ?;
-        `, [parcel_id]);
-
-        if (!parcel.length) {
-            console.error('Parcel not found:', parcel_id);
-            return false; 
-        }
-
         const total_price = parcel[0].parcel_unit_price;
         const warehouse_id = inventory[0].warehouse_id;
 
@@ -97,7 +96,7 @@ async function addPostalOrder(order_id, parcel_id) {
         `, [order_id, parcel_id, total_price]);
 
         if (insertResult.affectedRows > 0) {
-            // Update the parcel inventory quantity
+
             await InventoryService.updateParcelStockQuantity(parcel_id, warehouse_id, 0); // Setting quantity to 0 after order
             
             console.log(`Added new postal order for parcel_id: ${parcel_id}`);
@@ -117,17 +116,6 @@ async function addPostalOrder(order_id, parcel_id) {
 // add product order
 async function addProductOrder(order_id, product_id, product_quantity) {
     try {
-        const [inventory] = await db.query(`
-            SELECT warehouse_id, quantity
-            FROM product_inventories
-            WHERE product_id = ?;
-        `, [product_id]);
-
-        if (!inventory.length || inventory[0].quantity < product_quantity) {
-            console.error('Insufficient inventory for product:', product_id);
-            return false; 
-        }
-
         const [product] = await db.query(`
             SELECT product_unit_price
             FROM products
@@ -139,8 +127,20 @@ async function addProductOrder(order_id, product_id, product_quantity) {
             return false;
         }
 
+        const [inventory] = await db.query(`
+            SELECT warehouse_id, quantity
+            FROM product_inventories
+            WHERE product_id = ? ORDER BY quantity DESC LIMIT 1; 
+        `, [product_id]);
+
+        if (!inventory.length || inventory[0].quantity < product_quantity) {
+            console.error('Insufficient inventory for product:', product_id);
+            return false; 
+        }
+
         const unit_price = product[0].product_unit_price;
         const total_price = unit_price * product_quantity;
+        const warehouse_id = inventory[0].warehouse_id;
 
         const [insertResult] = await db.query(`
             INSERT INTO product_orders (order_id, product_id, product_quantity, total_price) 
@@ -148,12 +148,13 @@ async function addProductOrder(order_id, product_id, product_quantity) {
         `, [order_id, product_id, product_quantity, total_price]);
 
         if (insertResult.affectedRows > 0) {
-            const warehouse_id = inventory[0].warehouse_id;
+
             await InventoryService.updateProductStockQuantity(product_id, warehouse_id, inventory[0].quantity - product_quantity);
             console.log(`Added new product order for product_id: ${product_id}`);
             const log_message = `Added new product order with product_id ${product_id}`;
             await logger.addOrderLog(order_id, log_message);
             return true;
+
         } else {
             console.error('Failed to insert product order:', { order_id, product_id });
             return false;
@@ -167,10 +168,19 @@ async function addProductOrder(order_id, product_id, product_quantity) {
 // remove postal order
 async function removePostalOrder(order_id, parcel_id) {
     try {
+        const [inventory] = await db.query(`
+            SELECT warehouse_id, quantity
+            FROM parcel_inventories
+            WHERE parcel_id = ? ORDER BY quantity DESC LIMIT 1;
+        `);
+
+        const warehouse_id = inventory[0].warehouse_id;
+
         const [result] = await db.query(`
             DELETE FROM postal_orders WHERE order_id = ? AND parcel_id = ?;
         `, [order_id, parcel_id]);
         if (result.affectedRows > 0) {
+            await InventoryService.updateParcelStockQuantity(parcel_id, warehouse_id, 0);
             console.log('Unassigned parcel to order.')
             const log_message = `Unassigned parcel ${parcel_id} from order ${order_id}.`;
             await logger.addOrderLog(order_id, log_message);
@@ -187,10 +197,24 @@ async function removePostalOrder(order_id, parcel_id) {
 // remove product order
 async function removeProductOrder(order_id, product_id) {
     try {
+        const [inventory] = await db.query(`
+            SELECT warehouse_id, quantity
+            FROM product_inventories
+            WHERE product_id = ? ORDER BY quantity DESC LIMIT 1;
+        `);
+        const [reserved] = await db.query(`
+            SELECT product_quantity
+            FROM product_orders
+            WHERE order_id = ? AND product_id = ?
+            `, [order_id, product_id]);
+        const warehouse_id = inventory[0].warehouse_id;
+        const reservedQuantity =  reserved[0].product_quantity;
+
         const [result] = await db.query(`
             DELETE FROM product_orders WHERE order_id = ? AND product_id = ?;
         `, [order_id, product_id]);
         if (result.affectedRows > 0) {
+            await InventoryService.updateProductStockQuantity(product_id, warehouse_id, inventory.quantity + reservedQuantity);
             console.log('Unassigned product to order.')
             const log_message = `Unassigned product ${product_id} from order ${order_id}.`;
             await logger.addOrderLog(order_id, log_message);
